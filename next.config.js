e6a636cb0e7ffb0f6eb2b559ce74bdf7353f2911
Marika <temp@Centralitys-MBP.fritz.box>
2020-12-03 13:20:17 +1300

    create .env

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
 const path = require('path')
 
-module.exports = {
-    webpack: config => {
-        config.resolve.alias['components'] = path.join(__dirname, 'components')
-        config.resolve.alias['public'] = path.join(__dirname, 'public')
+require('dotenv').config()
 
-        return config
-    }
+module.exports = {
+  env: {
+    API_URL: process.env.API_URL,
+  },
+
+  webpack: config => {
+    config.resolve.alias.components = path.join(__dirname, 'components')
+    config.resolve.alias.public = path.join(__dirname, 'public')
+
+    return config
+  },
 }
